d00ccfec0b74f3ac92f3f9d268a63e18178e91bc
Lokeshranjan8 <lokesh.ranjan.phe23@itbhu.ac.in>
2025-04-25 18:41:05 +0530

    UPDATED COMMENTS

--- Memppol/mempool-btcd-2gb.sh
+++ Memppol/mempool-btcd-2gb.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# I successfully tested filling up the mempool using btcd and btcwallet by 
+# generating and sending a large number of transactions through bash scripts. 
+# The process was functionally successful, but I observed that it takes a significant amount of time to fill up the mempool 
+# — especially when aiming for a size close to 2GB
+
+
+
 BTCCTL_CMD="btcctl --regtest --rpcserver=127.0.0.1:18332 -u rpcuser -P rpcpass --wallet --notls"
 LIMIT_BYTES=$((2 * 1024 * 1024 * 1024))
 AVG_TX_SIZE=600  
@@ -10,7 +17,7 @@ $BTCCTL_CMD walletpassphrase "lokesh" 99999999 &
 
 for ((i = 1; i <= 100000; i++)); do
   ADDR=$($BTCCTL_CMD getnewaddress)
-  $BTCCTL_CMD sendtoaddress "$ADDR" 0.0001 > /dev/null &
+  $BTCCTL_CMD sendtoaddress "$ADDR" 0.001 > /dev/null &
 
   if (( i % CHECK_INTERVAL == 0 )); then
     wait 
@@ -27,4 +34,12 @@ for ((i = 1; i <= 100000; i++)); do
   fi
 done
 
+
+
+# //TOO SLOW PROCESS
+# btcctl --regtest --rpcserver=127.0.0.1:18332   -u rpcuser -P rpcpass --wallet --notls getmempoolinfo
+# {
+#   "size": 501,
+#   "bytes": 112942
+# }
 
